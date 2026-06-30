3a0e95054980282576489d0dcd807229132ab141
Nidhin Xavier <132472527+NDX7@users.noreply.github.com>
2026-06-30 18:57:17 +0530

    Update (7)revnumberv2.py

--- (7)revnumberv2.py
+++ (7)revnumberv2.py
@@ -1,16 +1,22 @@
 class Solution(object):
     def reverse(self, x):
-        if x>0:
-            x=str(x)
-            rx=x[::-1]
-            rx=int(rx)
-            return(rx)
-        else:
-            x=abs(x)
-            x=str(x)
-            rx=x[::-1]
-            rx=int(rx)
-            return(0-rx)
+        if x > 0:
+            x = str(x)
+            rx = x[::-1]
+            rx = int(rx)
             
-        
-        
+            
+            if rx > 2147483647:
+                return 0
+            return rx
+        else:
+            x = abs(x)
+            x = str(x)
+            rx = x[::-1]
+            rx = int(rx)
+            final_rx = 0 - rx
+            
+            
+            if final_rx < -2147483648:
+                return 0
+            return final_rx
